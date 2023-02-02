ffb52614655e986f938d6066fa4d7a25ccf6aa35
Diego Hernan Veron <diegohv74@gmail.com>
2023-02-02 19:50:15 -0300

    Modificado 02/02

--- Desarrollo-Frontend/function.js
+++ Desarrollo-Frontend/function.js
@@ -1,4 +1,4 @@
-//Funcion de pop up para boton de WhatsApp
+// Funcion de pop up para boton de WhatsApp
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
